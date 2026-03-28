ae52669f943dace27b846d85bef2f14eb55a7ba9
Jacob Greene <jacobrobert.greene@calbaptist.edu>
2026-03-27 18:22:52 -0700

    updates, started implementing bluetooth

--- main.cpp
+++ main.cpp
@@ -5,6 +5,108 @@
 #include <Adafruit_Seesaw.h>
 
 /////////////////////////////////////////////////////////////////////////
+//Bluetooth variables
+/////////////////////////////////////////////////////////////////////////
+BLEServer *bleServer;
+BLEService *bleService;
+BLECharacteristic *bleCharacteristic_S;
+BLECharacteristic *bleCharacteristic_C;
+bool deviceConnected = false;
+bool previouslyConnected = false;
+int timer = 0;
+
+// See the following for generating UUIDs: https://www.uuidgenerator.net/
+#define SERVICE_UUID "74be116b-2158-4c76-a579-0980aaa415d3"
+#define CHARACTERISTIC_UUID_S "7dcf8cb1-a9b1-44e6-bed5-3c2ffe02bb07"
+#define CHARACTERISTIC_UUID_C "41755070-8e22-4588-bf9b-80dd3ac3f42f"
+
+// BLE Broadcast Name
+static String BLE_BROADCAST_NAME = "M5 Core2 Tanks Game";
+
+///////////////////////////////////////////////////////////////
+// BLE Server Callback Methods
+///////////////////////////////////////////////////////////////
+class MyServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer *pServer) {
+        deviceConnected = true;
+        previouslyConnected = true;
+        Serial.println("Device connected...");
+    }
+    void onDisconnect(BLEServer *pServer) {
+        deviceConnected = false;
+        Serial.println("Device disconnected...");
+    }
+};
+
+///////////////////////////////////////////////////////////////
+// BLECharacteristic Callback Methods
+///////////////////////////////////////////////////////////////
+class MyCharacteristicCallbacks : public BLECharacteristicCallbacks {
+    // Callback function to support a read request.
+    void onRead(BLECharacteristic* pCharacteristic) {
+        String characteristicUUID = pCharacteristic->getUUID().toString().c_str();
+        String chraracteristicValue = pCharacteristic->getValue().c_str();
+        Serial.printf("Client JUST read from %s: %s", characteristicUUID.c_str(), chraracteristicValue.c_str());
+    }
+
+    // Callback function to support a write request.
+    void onWrite(BLECharacteristic* pCharacteristic) {
+        // Get the characteristic enum and print for logging
+        String characteristicUUID = pCharacteristic->getUUID().toString().c_str();
+        String chraracteristicValue = pCharacteristic->getValue().c_str();
+        Serial.printf("Client JUST wrote to %s: %s", characteristicUUID.c_str(), chraracteristicValue.c_str());
+
+        // TODO: Take action by checking if the characteristicUUID matches a known UUID
+        if (characteristicUUID.equalsIgnoreCase(CHARACTERISTIC_UUID_S)) {
+          // The characteristicUUID that just got written to by a client matches the known
+          // CHARACTERISTIC_UUID (assuming this constant is defined somewhere in our code)
+        }
+    }
+
+    // Callback function to support a Notify request.
+    void onNotify(BLECharacteristic* pCharacteristic) {
+        String characteristicUUID = pCharacteristic->getUUID().toString().c_str();
+        Serial.printf("Client JUST notified about change to %s: %s", characteristicUUID.c_str(), pCharacteristic->getValue().c_str());
+    }
+
+    // Callback function to support when a client subscribes to notifications/indications.
+    void onSubscribe(BLECharacteristic* pCharacteristic, uint16_t subValue) {
+    }
+
+    // Callback function to support a Notify/Indicate Status report.
+    void onStatus(BLECharacteristic* pCharacteristic, Status s, uint32_t code) {
+        // Print appropriate response
+		String characteristicUUID = pCharacteristic->getUUID().toString().c_str();
+        switch (s) {
+            case SUCCESS_INDICATE:
+                // Serial.printf("Status for %s: Successful Indication", characteristicUUID.c_str());
+                break;
+            case SUCCESS_NOTIFY:
+                Serial.printf("Status for %s: Successful Notification", characteristicUUID.c_str());
+                break;
+            case ERROR_INDICATE_DISABLED:
+                Serial.printf("Status for %s: Failure; Indication Disabled on Client", characteristicUUID.c_str());
+                break;
+            case ERROR_NOTIFY_DISABLED:
+                Serial.printf("Status for %s: Failure; Notification Disabled on Client", characteristicUUID.c_str());
+                break;
+            case ERROR_GATT:
+                Serial.printf("Status for %s: Failure; GATT Issue", characteristicUUID.c_str());
+                break;
+            case ERROR_NO_CLIENT:
+                Serial.printf("Status for %s: Failure; No BLE Client", characteristicUUID.c_str());
+                break;
+            case ERROR_INDICATE_TIMEOUT:
+                Serial.printf("Status for %s: Failure; Indication Timeout", characteristicUUID.c_str());
+                break;
+            case ERROR_INDICATE_FAILURE:
+                Serial.printf("Status for %s: Failure; Indication Failure", characteristicUUID.c_str());
+                break;
+        }
+    }
+};
+
+/////////////////////////////////////////////////////////////////////////
 //Joystick/Gamepad Buttons variables
 /////////////////////////////////////////////////////////////////////////
 Adafruit_seesaw ss = Adafruit_seesaw(&Wire);
@@ -95,8 +197,10 @@ const int largeDmg = 35;
 
 const float gravity = 0.5;
 const float projOffset = 20;
-float projX = 0;
-float projY = 300; //init beyond screen
+float projX1 = 0;
+float projY1 = 300; //init beyond screen
+float projX2 = 0;
+float projY2 = 300;
 int projSizeR = 0;
 float vx = 0;
 float vy = 0;
@@ -107,6 +211,11 @@ bool collision;
 /////////////////////////////////////////////////////////////////////////
 //functions
 /////////////////////////////////////////////////////////////////////////
+//bluetooth functions
+void broadcastBleServer();
+void readData();
+void sendData();
+
 //joystick functions
 void setupJoystick();
 
@@ -341,8 +450,8 @@ void shoot(uint16_t color, int xPos, int angle, Power power){
     float angleRad = angle * DEG_TO_RAD;
 
     //init projectile at tip of barrel X and Y pos
-    projX = xPos + (projOffset+projSizeR/2) * cos(angleRad); 
-    projY = tankBarrelY - (projOffset+projSizeR/2) * sin(angleRad);
+    projX1 = xPos + (projOffset+projSizeR/2) * cos(angleRad); 
+    projY1 = tankBarrelY - (projOffset+projSizeR/2) * sin(angleRad);
     
     vx = speed * cos(angleRad);
     vy = -speed * sin(angleRad);
@@ -357,15 +466,15 @@ void projectileMotion(uint16_t color, Power power){
   collision = false;
   while(!collision){
     //calculate new position and velocity
-    projX += vx;
-    projY += vy;
+    projX1 += vx;
+    projY1 += vy;
 
     vy += gravity;  // gravity pulls down
 
-    Serial.printf("projX: %f - projY: %f\n", projX, projY); //debug
+    Serial.printf("projX: %f - projY: %f\n", projX1, projY1); //debug
 
     // draw projectile in new position
-    M5.Lcd.fillCircle(projX, projY, projSizeR, color);
+    M5.Lcd.fillCircle(projX1, projY1, projSizeR, color);
 
     //check for collision
     checkCollision(power);
@@ -374,21 +483,21 @@ void projectileMotion(uint16_t color, Power power){
     delay(50);
 
     //erase previous projectile
-    M5.Lcd.fillCircle(projX, projY, projSizeR, skyColor);
+    M5.Lcd.fillCircle(projX1, projY1, projSizeR, skyColor);
   }
 }
 
 //check collisions
 void checkCollision(Power power){
   //check if out of screen
-  if(projX > sWidth || projX < 0){
+  if(projX1 > sWidth || projX1 < 0){
     Serial.println("MISS, out of screen");
     collision = true;
     return;
   }
 
   //check for collision with ground
-  if(projY >= floorHeight){
+  if(projY1 >= floorHeight){
     //vibrate
     M5.Axp.SetVibration(true);
     delay(100);
@@ -414,11 +523,11 @@ void checkCollision(Power power){
   int hitBoxBottom = floorHeight;
 
   // --- Check collision (box) ---
-  bool hitX = (projX >= hitBoxLeft && projX <= hitBoxRight);
-  bool hitY = (projY >= hitBoxTop  && projY <= hitBoxBottom);
+  bool hitX = (projX1 >= hitBoxLeft && projX1 <= hitBoxRight);
+  bool hitY = (projY1 >= hitBoxTop  && projY1 <= hitBoxBottom);
 
   Serial.printf("proj size: %d\n");
-  Serial.printf("proj(%.1f, %.1f) | tank: (%.1f, %.1f)\n", projX, projY, targetX, floorHeight - tankSizeY);
+  Serial.printf("proj(%.1f, %.1f) | tank: (%.1f, %.1f)\n", projX1, projY1, targetX, floorHeight - tankSizeY);
   Serial.printf("inX: %d inY: %d\n", hitX, hitY);
 
   if(hitX && hitY){
@@ -515,4 +624,95 @@ void drawGameOver(bool whoWon){
   } else {
     M5.Lcd.println("Player 2 Wins!");
   }
+}
+
+///////////////////////////////////////////////////////////////
+// Bluetooth Server
+///////////////////////////////////////////////////////////////
+void broadcastBleServer() {    
+    // Initializing the server, a service and a characteristic 
+    bleServer = BLEDevice::createServer();
+    bleServer->setCallbacks(new MyServerCallbacks());
+    bleService = bleServer->createService(SERVICE_UUID);
+
+    //initialize server characteristic
+    bleCharacteristic_S = bleService->createCharacteristic(CHARACTERISTIC_UUID_S,
+        BLECharacteristic::PROPERTY_READ |
+        BLECharacteristic::PROPERTY_WRITE |
+        BLECharacteristic::PROPERTY_NOTIFY |
+        BLECharacteristic::PROPERTY_INDICATE
+    );
+    bleCharacteristic_S->setCallbacks(new MyCharacteristicCallbacks());
+    bleCharacteristic_S->setValue("Hello from Jacob -> This is the server characteristic");
+    bleService->start();
+
+    //initialize client characteristic
+    bleCharacteristic_C = bleService->createCharacteristic(CHARACTERISTIC_UUID_C,
+        BLECharacteristic::PROPERTY_READ |
+        BLECharacteristic::PROPERTY_WRITE |
+        BLECharacteristic::PROPERTY_NOTIFY |
+        BLECharacteristic::PROPERTY_INDICATE
+    );
+    bleCharacteristic_C->setCallbacks(new MyCharacteristicCallbacks());
+    bleCharacteristic_C->setValue("Hello from Jacob -> This is the client characteristic");
+    bleService->start();
+
+    // Start broadcasting (advertising) BLE service
+    BLEAdvertising *bleAdvertising = BLEDevice::getAdvertising();
+    bleAdvertising->addServiceUUID(SERVICE_UUID);
+    bleAdvertising->setScanResponse(true);
+    bleAdvertising->setMinPreferred(0x12); // Use this value most of the time 
+    // bleAdvertising->setMinPreferred(0x06); // Functions that help w/ iPhone connection issues 
+    // bleAdvertising->setMinPreferred(0x00); // Set value to 0x00 to not advertise this parameter
+    BLEDevice::startAdvertising();
+    Serial.println("Characteristic defined...you can connect with your phone!"); 
+
+}
+
+void readData()
+{
+  //read from ble
+  //data from player2 client player
+
+  // Read the characteristic's value as a string (which can be written from a client)
+  std::string readValue = bleCharacteristic_C->getValue();
+  Serial.printf("Client Characteristic Value (player 2 Position): %s\n", readValue.c_str()); //format xxx-yyy
+  String str = readValue.c_str();
+
+  // parse the data
+  
+  //format: gameState_color-health=power+tankPosX>projPosX,projPosY
+  //ranges: (0-1)_(0-3)-(0-100)=(0-2)+(0-320)>(0-320,0-240)
+
+  //gameState controlled, not received by server
+  tankColor2 = str.substring(str.indexOf("_"), str.indexOf("-")).toInt();
+  tankHealth2 = str.substring(str.indexOf("-"), str.indexOf("=")).toInt();
+  int temp = str.substring(str.indexOf("="), str.indexOf("+")).toInt();
+  if(temp == 0){
+    tankPower2 = SMALL;
+  } else if(temp == 1){
+    tankPower2 = MEDIUM;
+  } else if(temp == 2){
+    tankPower2 = LARGE;
+  }
+  tankPos2 = str.substring(str.indexOf("+"), str.indexOf(">")).toInt();
+
+  projX2 = str.substring(str.indexOf(">"), str.indexOf(",")).toInt();
+  projY2 = str.substring(str.indexOf(","), str.indexOf(")")).toInt();
+}
+
+void sendData(){
+  // write to ble
+  //data for player1 Server player
+
+  //format: gameState_color-health=power+tankPosX>projPosX,projPosY)
+  //ranges: (0-1)_(0-3)-(0-100)=(0-2)+(0-320)>(0-320,0-240)
+
+  //convert to string
+  String str = String(gameState) + "_" + String(tankColor1) + "-" + String(tankHealth1) + "=" + String(tankPower1) + "+" + String(tankPos1) + ">" + String(projX1) + "," + String(projY1);
+
+  // write the charactistic's value as a string (which can be read by a client)
+  bleCharacteristic_S->setValue(str.c_str());
+  bleCharacteristic_S->notify();
+  Serial.printf("%d Server characteristic value (player 1 Position): %s\n", str.c_str());
 }
